b438c8f65090042bbf29cab93a838c9156f1ff66
pom421 <mauguet.po@gmail.com>
2024-03-20 11:09:04 +0100

    feat: test with better-sqlite3 instead of sqlite3 WIP

--- api/country.js
+++ api/country.js
@@ -1,11 +1,10 @@
 const {join} = require('path');
 const {now} = require('perf_hooks').performance;
+
 const ip2location = join(__dirname, '..', 'sqlite', 'ip2location.db');
 
-
 const db = require('better-sqlite3')(ip2location);
-const SQLiteTag = require('sqlite-tag');
-
+// const SQLiteTag = require('sqlite-tag');
 
 const ipv4 = {
   _: [16777216, 65536, 256, 1],
@@ -14,23 +13,27 @@ const ipv4 = {
   asRegExp: /^(?:\d+\.){3}\d+$/
 };
 
-module.exports = async (req, res) => {
+const sql = "SELECT country_code AS code, country_name AS name FROM ip2location limit 10";
+
+module.exports = (req, res) => {
   const address = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   if (ipv4.asRegExp.test(address)) {
     const time = now();
     // const db = new Database(ip2location);
-    const {get} = SQLiteTag(db);
-    const location = await get`
-      SELECT
-        country_code AS code,
-        country_name AS name
-      FROM
-        ip2location
-      WHERE
-        ${ipv4.asInt(address)}
-      BETWEEN ip_from AND ip_to
-    `;
-    db.close();
+    // const {get} = SQLiteTag(db);
+    // const location = await get`
+    //   SELECT
+    //     country_code AS code,
+    //     country_name AS name
+    //   FROM
+    //     ip2location
+    //   WHERE
+    //     ${ipv4.asInt(address)}
+    //   BETWEEN ip_from AND ip_to
+    // `;
+    // db.close();
+
+    const location = db.prepare(sql).all();
     res.setHeader('x-served-in', `${(now() - time).toFixed(2)}ms`);
     res.json(location || {code: '❔', name: 'Unknown'});
   }
